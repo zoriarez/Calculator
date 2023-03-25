e4e471daa7f7c5a38ee11cb86e17dbb8dc509647
zoriarez <127724840+zoriarez@users.noreply.github.com>
2023-03-26 06:51:59 +0800

    numpad created

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-let numbs = ['0','1','2','3','4','5','6','7','8','9']
-let one = document.getElementById('1');
+
+let numberContainer = document.getElementById('numberContainer');
 let two = document.getElementById('2');
 let text = document.getElementById('text');
 let stringg;
 let additional 
-console.log(numbs[0])
+//console.log(numbs[0])
 
 
+for(let i=0; i<9; i++){
+    let numKeys = document.createElement('div');
+   // numKeys.setAttribute('type','button');
+    numKeys.classList.add('numKeys');
+    numberContainer.style.gridTemplateColumns = `repeat(3,auto)`
+    numberContainer.appendChild(numKeys)
+    numKeys.value = i +1;
+    numKeys.innerHTML = i + 1;
+
+    numKeys.addEventListener('click',()=>{
+        console.log(numKeys.value)
+    })
+}
+
+/*
 console.log(text.value)
 one.addEventListener('click',()=>{
     stringg = `${numbs[1]}`
      text.value= text.value + stringg
     console.log(text.value)
 
-    /*if(stringg.length == 0){
+    if(stringg.length == 0){
 
-    }*/
+    }
 })
 
 two.addEventListener('click',()=>{
     text.value = `${numbs[2]}${text.value}`
     console.log(stringg)
-
+*/
     /*if(stringg.length == 0){
 
-    }*/
+    }
 })
+*/
