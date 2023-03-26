f05c1ab6e21f0179e7c40a81fd37ae3e2b7e13e1
zoriarez <127724840+zoriarez@users.noreply.github.com>
2023-03-26 08:24:57 +0800

    text display with pushable numbers

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 
 let numberContainer = document.getElementById('numberContainer');
-let two = document.getElementById('2');
-let text = document.getElementById('text');
+let Clear = document.getElementById('C');
+let divide = document.getElementById('divide');
+let  multiply= document.getElementById('multiply');
+let minus = document.getElementById('minus');
+let  plus = document.getElementById('plus');
+let  equals = document.getElementById('=');
+let textDisplay = document.getElementById('textDisplay');
 let stringg;
 let additional 
 //console.log(numbs[0])
 
+//textDisplay.innerHTML = 2443;
 
 for(let i=0; i<9; i++){
-    let numKeys = document.createElement('div');
-   // numKeys.setAttribute('type','button');
-    numKeys.classList.add('numKeys');
+    let numKeys = document.createElement('input');
+    numKeys.setAttribute('type','button');
+    numKeys.className="numKeys";
     numberContainer.style.gridTemplateColumns = `repeat(3,auto)`
     numberContainer.appendChild(numKeys)
     numKeys.value = i +1;
     numKeys.innerHTML = i + 1;
 
-    numKeys.addEventListener('click',()=>{
+   numKeys.addEventListener('click',()=>{
         console.log(numKeys.value)
+        textDisplay.value= numKeys.value
+        textDisplay.innerHTML +=numKeys.value
     })
 }
+
 
 /*
 console.log(text.value)
